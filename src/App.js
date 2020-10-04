34f729bb55af6cb65c379b666bfaa06be56eeb02
Akshat <akshatsaraswat03@gmail.com>
2020-10-05 00:22:41 +0530

    search bar added

--- src/App.js
+++ src/App.js
@@ -10,22 +10,50 @@ const SEARCHAPI =
 
 const App = ()=> {
     const [movies, setMovies] = useState([])
+    const [searchTerm, setSearch] = useState("")
     
     useEffect(()=> {
         fetch(APIURL).then(res => res.json())
         .then(data => {
-            console.log(data)
+            // console.log(data)
             setMovies(data.results)
         })
 
     }, [])
 
-    return (<div className="movie-container">
-        {movies.length > 0 && movies.map((movie) => 
-            <Movie key={movie.id} {...movie}/>
-        )}
-    </div>
-    )
-}
+    function handleOnSubmit(event) {
+        event.preventDefault();
+
+        if(searchTerm) {
+            fetch(SEARCHAPI + searchTerm).then(res => res.json())
+            .then(data => {
+                setMovies(data.results)
+            })
+
+            setSearch("");
+        }
+    }
+
+    function handleOnChange(event) {
+        setSearch(event.target.value)
+    }
+
+    return (
+        <>
+        <header>
+            <form onSubmit={handleOnSubmit}>
+                <input className="search" type="text" placeholder="Search..." onChange={handleOnChange} />
+            </form>
+            
+        </header>
+    
+        <div className="movie-container">
+            {movies.length > 0 && movies.map((movie) => 
+                <Movie key={movie.id} {...movie}/>
+            )}
+        </div>
+        )
+    </>
+    )}
 
 export default App
